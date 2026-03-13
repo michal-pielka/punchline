7f881dd7685b3d01842b4dc62300a6c68213a6cd
Michal Pielka <mpielka726@gmail.com>
2026-03-13 02:52:51 +0100

    feat(stund): add server ping pong

---
+++ crates/stund/src/main.rs
@@ -0,0 +1,18 @@
+use std::net::UdpSocket;
+
+const ADDRESS: &str = "0.0.0.0";
+const PORT: &str = "3478";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sock = UdpSocket::bind(format!("{}:{}", ADDRESS, PORT))?;
+    let mut buf = [0u8; 1024];
+
+    loop {
+        let (len, src_addr) = sock.recv_from(&mut buf).unwrap();
+        let request = &buf[..len];
+
+        if let Err(e) = sock.send_to(request, src_addr) {
+            println!("Sending error: {e}");
+        }
+    }
+}
