3ab91d27deac87d5e470517451a46679dbf81ecf
Michal Pielka <mpielka726@gmail.com>
2026-03-13 15:57:30 +0100

    feat(proto): add PairResponse struct

--- crates/proto/src/signal.rs
+++ crates/proto/src/signal.rs
@@ -1,10 +1,15 @@
 use std::net::SocketAddr;
 
-struct PairRequest {
+pub struct PairRequest {
     // Used to let the server know the "hole's" address
-    external_addr: SocketAddr,
+    pub external_addr: SocketAddr,
     // String for now, prolly needs a refactorings
     // Used to identify both parties of the communication - TODO: signatures
-    public_key: String,
-    target_public_key: String,
+    pub public_key: String,
+    pub target_public_key: String,
+}
+
+pub struct PairResponse {
+    pub target_external_addr: SocketAddr,
+    pub target_public_key: String,
 }
