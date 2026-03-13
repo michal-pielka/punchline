e672fd41d46ebcc405a385567476b4e961d14cdb
Michal Pielka <mpielka726@gmail.com>
2026-03-13 11:39:47 +0100

    refactor(client): move to stun.rs

---
+++ crates/client/src/stun.rs
@@ -0,0 +1,23 @@
+use std::net::UdpSocket;
+
+use punchline_proto::stun::{build_binding_request, parse_xor_mapped_address};
+
+const ADDRESS: &str = "0.0.0.0:0";
+
+fn get_external_address() -> Result<(), Box<dyn std::error::Error>> {
+    let server_address = std::env::var("SERVER_ADDRESS")?;
+
+    let sock = UdpSocket::bind(ADDRESS)?;
+    let _ = sock.connect(server_address);
+
+    let (request, transaction_id) = build_binding_request();
+    let _ = sock.send(&request);
+
+    let mut buf = [0u8; 1024];
+    let len = sock.recv(&mut buf)?;
+
+    let addr = parse_xor_mapped_address(&buf[..len]);
+    println!("Public address: {addr:?}");
+
+    Ok(())
+}
