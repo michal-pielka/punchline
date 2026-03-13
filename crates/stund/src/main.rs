eed98728e5a8566badb73d70ce544975b6cd3c10
Michal Pielka <mpielka726@gmail.com>
2026-03-13 15:28:39 +0100

    feat(stund): migrate to UdpTransport

--- crates/stund/src/main.rs
+++ crates/stund/src/main.rs
@@ -1,12 +1,14 @@
 use std::net::UdpSocket;
 
 use punchline_proto::stun;
+use punchline_proto::transport::Transport;
+use punchline_proto::udp::UdpTransport;
 
 const ADDRESS: &str = "0.0.0.0";
 const PORT: &str = "3478";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let sock = UdpSocket::bind(format!("{}:{}", ADDRESS, PORT))?;
+    let sock = UdpTransport::new(UdpSocket::bind(format!("{}:{}", ADDRESS, PORT))?);
     let mut buf = [0u8; 1024];
 
     println!("STUN server listening on {ADDRESS}:{PORT}");
