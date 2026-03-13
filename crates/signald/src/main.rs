f82558053ca277a9b7a98fd2c617a130849f9238
Michal Pielka <mpielka726@gmail.com>
2026-03-13 16:09:44 +0100

    feat(signald): add thread per conn

--- crates/signald/src/main.rs
+++ crates/signald/src/main.rs
@@ -1,23 +1,29 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 use tungstenite::accept;
 
 const ADDRESS: &str = "0.0.0.0";
 const PORT: &str = "8743";
 
+fn handle_connection(stream: TcpStream) {
+    let mut ws = accept(stream).expect("WebSocket accept failed");
+
+    loop {
+        let msg = ws.read().expect("Read failed");
+
+        if msg.is_text() {
+            ws.send(msg).expect("Send failed");
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind(format!("{}:{}", ADDRESS, PORT))?;
 
-    if let Some(stream) = listener.incoming().next() {
+    for stream in listener.incoming() {
         let stream = stream?;
-        let mut ws = accept(stream)?;
 
-        loop {
-            let msg = ws.read()?;
-
-            if msg.is_text() {
-                ws.send(msg)?;
-            }
-        }
+        thread::spawn(move || handle_connection(stream));
     }
 
     Ok(())
