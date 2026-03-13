cbaf9c2cbc34939b7a29ca5416423d8ba48422cb
Michal Pielka <mpielka726@gmail.com>
2026-03-13 16:04:48 +0100

    feat(signald): add template echo server

--- crates/signald/src/main.rs
+++ crates/signald/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    todo!()
+use std::net::TcpListener;
+use tungstenite::accept;
+
+const ADDRESS: &str = "0.0.0.0";
+const PORT: &str = "8743";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind(format!("{}:{}", ADDRESS, PORT))?;
+
+    if let Some(stream) = listener.incoming().next() {
+        let stream = stream?;
+        let mut ws = accept(stream)?;
+
+        loop {
+            let msg = ws.read()?;
+
+            if msg.is_text() {
+                ws.send(msg)?;
+            }
+        }
+    }
+
+    Ok(())
 }
