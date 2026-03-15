4beedb020ac465699865e02efb83b575af951a28
Michal Pielka <mpielka726@gmail.com>
2026-03-15 22:13:36 +0100

    chore(client): swap ed25519 to x25519 keys, don't pass signature to signald

--- crates/client/src/signal.rs
+++ crates/client/src/signal.rs
@@ -1,26 +1,18 @@
 use std::net::SocketAddr;
 
-use ed25519_dalek::{SigningKey, VerifyingKey};
-use punchline_proto::{
-    crypto::sign_handshake,
-    signal::{PairRequest, PairResponse},
-};
+use punchline_proto::signal::{PairRequest, PairResponse};
 use tracing::{debug, info};
-use tungstenite;
 
 pub fn pair_with_peer(
-    signing_key: &SigningKey,
     external_addr: SocketAddr,
-    public_key: &VerifyingKey,
-    peer_public_key: &VerifyingKey,
+    public_key: &[u8; 32],
+    peer_public_key: &[u8; 32],
     signal_addr: SocketAddr,
 ) -> anyhow::Result<PairResponse> {
     debug!(%signal_addr, "Connecting to signal server");
     let (mut sock, _response) = tungstenite::connect(format!("ws://{}", signal_addr))?;
 
-    let signature = sign_handshake(signing_key, external_addr, public_key, peer_public_key);
-
-    let pair_request = PairRequest::new(external_addr, public_key, peer_public_key, &signature);
+    let pair_request = PairRequest::new(external_addr, public_key, peer_public_key);
 
     let json = serde_json::to_string(&pair_request)?;
     sock.send(tungstenite::Message::Text(json.into()))?;
